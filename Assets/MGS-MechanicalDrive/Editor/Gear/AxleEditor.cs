f14b80de8c8f839d34e055e0205e25ed03eb421d
Mogoson <mogoson@qq.com>
2018-06-14 00:13:31 +0800

    Modify file path.

---
+++ Assets/MGS-MechanicalDrive/Editor/Gear/AxleEditor.cs
@@ -0,0 +1,36 @@
+﻿/*************************************************************************
+ *  Copyright © 2017-2018 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  AxleEditor.cs
+ *  Description  :  Custom editor for Axle.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  6/13/2018
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Mogoson.Machinery
+{
+    [CustomEditor(typeof(Axle), true)]
+    [CanEditMultipleObjects]
+    public class AxleEditor : BaseMEditor
+    {
+        #region Field and Property
+        protected Axle Target { get { return target as Axle; } }
+        #endregion
+
+        #region Protected Method
+        protected virtual void OnSceneGUI()
+        {
+            Handles.color = Blue;
+            DrawSphereCap(Target.transform.position, Quaternion.identity, NodeSize);
+            DrawCircleCap(Target.transform.position, Target.transform.rotation, NodeSize + 0.025f);
+            DrawSphereArrow(Target.transform.position, Target.transform.forward, ArrowLength, NodeSize, Blue, "Axis");
+        }
+        #endregion
+    }
+}
