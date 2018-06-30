22d16bf7afda1387676e2f8105fbf8324fccae7f
Mogoson <mogoson@outlook.com>
2018-06-30 23:56:56 +0800

    Add Transmission demo.

--- Assets/MGS-MechanicalDrive/Scripts/UI/DifferentialUI.cs
+++ Assets/MGS-MechanicalDrive/Scripts/UI/DifferentialUI.cs
@@ -20,7 +20,6 @@ namespace Mogoson.Machinery
         #region Field and Property
         public float top = 10;
         public float left = 10;
-        public string info = "Help Info.";
 
         private Differential differential;
         private float coefficient = 0;
@@ -38,15 +37,12 @@ namespace Mogoson.Machinery
             GUILayout.BeginHorizontal();
             GUILayout.Space(left);
 
-            GUILayout.BeginVertical();
-            GUILayout.Label(info);
             var sliderValue = GUILayout.HorizontalSlider(coefficient, -2, 2, GUILayout.Width(240));
             if (coefficient != sliderValue)
             {
                 coefficient = sliderValue;
                 differential.Coefficient = coefficient;
             }
-            GUILayout.EndVertical();
 
             GUILayout.EndHorizontal();
         }
